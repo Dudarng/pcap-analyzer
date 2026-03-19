81fa2513d081eff3435d5eefc0748e6bee2294f4
Dudarng <mariaeduarda09847@gmail.com>
2026-03-18 22:50:50 -0300

    feat: adiciona api da abuseIPDB

--- enricher.py
+++ enricher.py
@@ -2,30 +2,109 @@ import os
 import requests
 from dotenv import load_dotenv
 
-# 1. Abre o cofre e carrega a chave secreta
 load_dotenv()
-# O nome dentro do getenv() tem que ser exatamente o que você escreveu no arquivo .env
 API_KEY_VIRUSTOTAL = os.getenv("API_KEY_VIRUSTOTAL") 
+API_KEY_IPDB = os.getenv("API_KEY_IPDB")
 
-# 2. A sua lógica de montagem
-ip_teste = "8.8.8.8"
-url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip_teste}"
-cabecalhos = {"x-apikey": API_KEY_VIRUSTOTAL}
+def getVirusTotal(ip):
+    url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
+    cabecalhos = {"x-apikey": API_KEY_VIRUSTOTAL}
 
-print(f"[*] Verificação para o IP: {ip_teste}...")
+    print(f"[*] Verificação para o IP: {ip}...")
 
-# 3. O "Botão de Enviar": Fazendo a requisição GET
-resposta = requests.get(url, headers=cabecalhos)
+    
+    try:
+        resposta = requests.get(url, headers=cabecalhos)
+    except Exception as erro_rede:
+        return f"[-] Erro de conexão ao tentar alcançar o VirusTotal para o IP {ip}: {erro_rede}"
 
-# 4. Verificando a resposta do servidor
-print(f"Status Code: {resposta.status_code}")
+    
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        as_owner = dados['data']['attributes'].get('as_owner', 'Desconhecido')
+        stats = dados['data']['attributes']['last_analysis_stats']
+        
+        return {
+            "dono": as_owner,
+            "malicioso": stats['malicious'],
+            "inofensivo": stats['harmless'],
+            "indetectavel": stats['undetected'],
+            "erro": None
+        }
+        
+    elif resposta.status_code == 401:
+        return f"[-] Erro 401: Acesso Negado no IP {ip}. Chave incorreta."
+    elif resposta.status_code == 429:
+        return f"[-] Erro 429: Limite de requisições excedido na API."
+    else:
+        return f"[-] Falha ao verificar IP {ip}. Código: {resposta.status_code}"
 
-if resposta.status_code == 200:
-    print("[+] Conexão aceita:")
-    # O método .json() pega a resposta bruta e transforma numa estrutura legível para o Python
-    dados_estruturados = resposta.json()
-    print(dados_estruturados)
-elif resposta.status_code == 401:
-    print("[-] Erro 401: Acesso Negado. Sua chave da API está incorreta ou não foi lida do .env.")
-else:
-    print(f"[-] Erro inesperado. Código: {resposta.status_code}")
+def getAbuseIPDB(ip):
+    url = "https://api.abuseipdb.com/api/v2/check"
+    cabecalhos = {
+        'Accept': 'application/json',
+        'Key': API_KEY_IPDB
+    }
+    parametros = {
+        'ipAddress': ip,
+        'maxAgeInDays': '90',
+        'verbose': '1'
+    }
+    attackType = {
+        1: "DNS Compromise",
+        2: "DNS Poisoning",
+        3: "Fraude (Fraud Orders)",
+        4: "Ataque DDoS",
+        5: "Força Bruta em FTP",
+        6: "Ping of Death",
+        7: "Phishing",
+        8: "Fraude em VoIP",
+        9: "Open Proxy",
+        10: "Spam em Fóruns/Web",
+        11: "Spam de Email",
+        12: "Spam em Blogs",
+        13: "IP de VPN (Comportamento Suspeito)",
+        14: "Varredura de Portas (Port Scan)",
+        15: "Hacking / Tentativa de Invasão",
+        16: "Injeção de SQL (SQLi)",
+        17: "Falsificação de Origem (Spoofing)",
+        18: "Ataque de Força Bruta (Geral)",
+        19: "Bot Malicioso (Bad Web Bot)",
+        20: "Host Comprometido/Infectado",
+        21: "Ataque a Aplicação Web",
+        22: "Força Bruta em SSH",
+        23: "Ataque a Dispositivos IoT"
+    }
+    try:
+       
+        resposta = requests.get(url, headers=cabecalhos, params=parametros)
+    except Exception as erro_rede:
+        return {"erro": f"Falha de rede ao contatar AbuseIPDB: {erro_rede}"}
+
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        
+        attackSearch = set()
+        reports = dados['data'].get('reports', [])
+        
+        for rep in reports:
+            for cat_id in rep.get('categories', []):
+                nameCat = attackType.get(cat_id, f"Outros ({cat_id})")
+                attackSearch.add(nameCat)
+        searchText = ", ".join(attackSearch) if attackSearch else "Não especificada"
+        
+      
+        return {
+            "score_abuso": dados['data']['abuseConfidenceScore'],
+            "total_reportes": dados['data']['totalReports'],
+            "pais": dados['data']['countryCode'],
+            "motivos": searchText, 
+            "erro": None
+        }
+    else:
+        return {"erro": f"Erro na API do AbuseIPDB. Código: {resposta.status_code}"}
+    
+
+if __name__ == "__main__":
+    resultado = getVirusTotal("8.8.8.8")
+    print(resultado)
--- extractor.py
+++ extractor.py
@@ -1,5 +1,7 @@
 import pyshark
 import ipaddress
+from enricher import getVirusTotal, getAbuseIPDB
+import time
 
 ips = set()
 
@@ -19,4 +21,29 @@ for packet in captura:
             ips.add(ipDestino) 
     except:
         continue 
-print(ips)
+print(f"Leitura do arquivo concluída")
+print(f"Total de IPs: {len(ips)}")
+print(f"Iniciando analise de IPs")
+
+for ip in ips:
+    vt = getVirusTotal(ip)
+    ipdb = getAbuseIPDB(ip)
+    
+    if vt.get("erro") or ipdb.get("erro"):
+        print(f"[-] Problema ao analisar {ip}: {vt.get('erro')} | {ipdb.get('erro')}")
+        time.sleep(16)
+        continue
+    
+    maliciosos_vt = vt['malicioso']
+    score_abuse = ipdb['score_abuso']
+    
+    if maliciosos_vt > 0 or score_abuse > 0:
+        print(f"[ALERTA] IP: {ip}")
+        print(f"Organização: {vt['dono']} (País: {ipdb['pais']})")
+        if maliciosos_vt > 0 :
+            print(f"{maliciosos_vt} motores classificaram como malicioso.")
+        print(f"Score de {score_abuse}% de confiança de abuso. ({ipdb['total_reportes']} denúncias recentes)")
+        print(f"Tipo de Ataque: {ipdb["motivos"]}\n")
+    time.sleep(16)
+    
+print("Analise concluida")
